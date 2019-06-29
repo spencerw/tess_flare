3cd720444e489a59e482d82498565b07883fd825
Spencer Wallace <scw7@uw.edu>
2019-06-29 14:31:04 -0700

    Fix up flare pipeline to match Jim's output

--- flarePipeline.py
+++ flarePipeline.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from astropy.table import Table
 import pandas as pd
 import numpy as np
 import exoplanet as xo
@@ -23,45 +24,43 @@ def procFlares(files, sector, makefig=True, clobber=False):
         filename = files[k].split('/')[-1]
         acf_1dt = 0
 
-        # Would be nice if I could open this with astropy.Table.read
-        # and turn it into a pandas dataframe
-        # When I try, it tells me 'IOError: Header missing END card.'
         with fits.open(files[k], mode='readonly') as hdulist:
             tess_bjd = hdulist[1].data['TIME']
             quality = hdulist[1].data['QUALITY']
             pdcsap_flux = hdulist[1].data['PDCSAP_FLUX']
             pdcsap_flux_error = hdulist[1].data['PDCSAP_FLUX_ERR']
             
-        ok_cut = (quality == 0)
+            tbl = Table([tess_bjd, quality, pdcsap_flux, pdcsap_flux_error], 
+                        names=('TIME', 'QUALITY', 'PDCSAP_FLUX', 'PDCSAP_FLUX_ERR'))
+            df_tbl = tbl.to_pandas()
+            
+        ok_cut = (tbl['QUALITY'] == 0)
         
-        median = np.nanmedian(pdcsap_flux[ok_cut])
-        acf = xo.autocorr_estimator(tess_bjd[ok_cut], pdcsap_flux[ok_cut]/median,
-                                    yerr=pdcsap_flux_error[ok_cut]/median,
+        median = np.nanmedian(df_tbl['PDCSAP_FLUX'][ok_cut])
+        acf = xo.autocorr_estimator(tbl['TIME'][ok_cut], tbl['PDCSAP_FLUX'][ok_cut]/median,
+                                    yerr=tbl['PDCSAP_FLUX_ERR'][ok_cut]/median,
                                     min_period=0.1, max_period=27, max_peaks=2)
         
         if len(acf['peaks']) > 0:
             acf_1dt = acf['peaks'][0]['period']
             mask = np.where((acf['autocorr'][0] == acf['peaks'][0]['period']))[0]
             acf_1pk = acf['autocorr'][1][mask][0]
-            s_window = int(acf_1dt/np.fabs(np.nanmedian(np.diff(tess_bjd))) / 6)
+            s_window = int(acf_1dt/np.fabs(np.nanmedian(np.diff(df_tbl['TIME']))) / 6)
         else:
             s_window = 128
         
-        # This is annoying, since I can't create a dataframe from the FITS data, I have
-        # to do all of this extra crap
-        smo = pd.DataFrame(pdcsap_flux[ok_cut]).rolling(s_window, center=True).median().values
-        smo = smo.reshape(1, -1)[0]
+        smo = df_tbl['PDCSAP_FLUX'].rolling(s_window, center=True).median()
 
         if makefig:
             fig, axes = plt.subplots(figsize=(8,8))
-            axes.errorbar(tess_bjd[ok_cut], pdcsap_flux[ok_cut]/median,
-                          yerr=pdcsap_flux_error[ok_cut]/median, 
+            axes.errorbar(df_tbl['TIME'][ok_cut], df_tbl['PDCSAP_FLUX'][ok_cut]/median,
+                          yerr=df_tbl['PDCSAP_FLUX_ERR'][ok_cut]/median, 
                           linestyle=None, alpha=0.15, label='PDCSAP_FLUX')
-            axes.plot(tess_bjd[ok_cut], smo/median, label=str(s_window)+'pt median')
+            axes.plot(df_tbl['TIME'][ok_cut], smo/median, label=str(s_window)+'pt median')
 
             if (acf_1dt > 0):
-                y = np.nanstd(smo/median)*acf_1pk*np.sin(tess_bjd[ok_cut]/acf_1dt*2*np.pi) + 1
-                axes.plot(tess_bjd[ok_cut], y, lw=2, alpha=0.7,
+                y = np.nanstd(smo/median)*acf_1pk*np.sin(df_tbl['TIME'][ok_cut]/acf_1dt*2*np.pi) + 1
+                axes.plot(df_tbl['TIME'][ok_cut], y, lw=2, alpha=0.7,
                          label='ACF='+format(acf_1dt,'6.3f')+'d, pk='+format(acf_1pk,'6.3f'))
             
         if np.sum(ok_cut) < 1000:
@@ -69,9 +68,9 @@ def procFlares(files, sector, makefig=True, clobber=False):
             
         sok_cut = np.isfinite(smo)
         
-        FL = FINDflare(pdcsap_flux[ok_cut][sok_cut] - smo[sok_cut]/median, 
-                       pdcsap_flux_error[ok_cut][sok_cut]/median,
-                       N1=4, N2=2, N3=5, avg_std=False)
+        FL = FINDflare((df_tbl['PDCSAP_FLUX'][ok_cut][sok_cut] - smo[sok_cut])/median, 
+                       df_tbl['PDCSAP_FLUX_ERR'][ok_cut][sok_cut]/median,
+                       avg_std=False, N1=4, N2=2, N3=5)
         
         for j in range(len(FL[0])):
             FL_id = np.append(FL_id, k)
@@ -79,11 +78,11 @@ def procFlares(files, sector, makefig=True, clobber=False):
             FL_t1 = np.append(FL_t1, FL[1][j])
             FL_f0 = np.append(FL_f0, median)
             s1, s2 = FL[0][j], FL[1][j]+1
-            FL_f1 = np.append(FL_f1, np.nanmax(pdcsap_flux[ok_cut][sok_cut][s1:s2]))
+            FL_f1 = np.append(FL_f1, np.nanmax(df_tbl['PDCSAP_FLUX'][ok_cut][sok_cut][s1:s2]))
             
             if makefig:
-                axes.scatter(tess_bjd[ok_cut][sok_cut][s1:s2],
-                             pdcsap_flux[ok_cut][sok_cut][s1:s2]/median,
+                axes.scatter(df_tbl['TIME'][ok_cut][sok_cut][s1:s2],
+                             df_tbl['PDCSAP_FLUX'][ok_cut][sok_cut][s1:s2]/median,
                              color='r', label='_nolegend_')
                 axes.scatter([],[], color='r', label='Flare?')        
 
@@ -96,8 +95,7 @@ def procFlares(files, sector, makefig=True, clobber=False):
             figname = filename + '.jpeg'
             makefig = ((not os.path.exists(figname)) | clobber)
             plt.savefig(figname, bbox_inches='tight', pad_inches=0.25, dpi=100)
-            plt.close()
-        
+            plt.close()      
         
         update_progress(k / len(files))
         
