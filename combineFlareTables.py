add466e0b207a0899be33ea075dde47dfdea37f1
spencerw <scw7@uw.edu>
2019-08-06 12:32:34 -0700

    Fix bug where extra columns added when combining flare tables

--- combineFlareTables.py
+++ combineFlareTables.py
@@ -4,5 +4,11 @@ import glob as gl
 all_files = gl.glob('*_flare_out.csv')
 prefix = all_files[0].split('.p')[0]
 
-data = pd.concat((pd.read_csv(f) for f in all_files))
+data = []
+
+for f in all_files:
+    df = pd.read_csv(f)
+    data.append(df)
+
+data = pd.concat(data)
 data.to_csv(prefix+ '_flare_out.csv')
