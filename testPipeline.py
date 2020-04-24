d11705344ab9d1e0e4beae5a2994a283fe943400
Spencer Wallace <scw7@uw.edu>
2020-04-24 16:49:46 -0700

    Mask out other flares when fitting

--- testPipeline.py
+++ testPipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 from flareFind import procFlares
 
-path = 'test_files/'
+path = 'test/'
 filenames = np.genfromtxt(path + 'test_files.txt', comments='#', dtype='str')
 procFlares('test', filenames, path, makePlots=True, clobberGP=False)
