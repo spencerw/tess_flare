c988893ff9289eb922a786b3ebb4eb97862284f8
Spencer Wallace <scw7@uw.edu>
2020-10-01 14:18:04 -0700

    Use isochrone model to select main sequence targets

--- procData.py
+++ procData.py
@@ -10,8 +10,8 @@ from flareTools import printProgressBar
 # Calculate luminosities by estimating TESS mag from GAIA color
 # Filter flares based on flare model fits
 
-prefix = '14to26'
-path = '/astro/store/gradscratch/tmp/scw7/tessData/lightcurves/sec1to13/'
+prefix = 'allsky'
+path = '/astro/store/gradscratch/tmp/scw7/tessData/lightcurves/' + prefix + '/'
 clobber = False
 
 print('Processing data for ' + prefix)
@@ -51,7 +51,7 @@ else:
 #####################################################################
 print('Crossmatching GAIA and TESS targets')
 
-sectors = [pd.read_csv('TESS-Gaia/gaiatess{0}_xmatch_1arsec-result.csv'.format(n+1)) for n in range(26)]
+sectors = [pd.read_csv('TESS-Gaia/gaiatess{0}_xmatch_1arsec-result.csv'.format(n+1)) for n in range(27)]
 data = pd.concat(sectors, sort=True)
 
 df_param = pd.read_csv(prefix + '_rot_param_out.csv')
@@ -76,14 +76,21 @@ iso = iso_table1
 T = iso['TESSmag'].values[::-1]
 G_B = iso['G_BPbrmag'].values[::-1]
 G_R = iso['G_RPmag'].values[::-1]
-GminR = G_B - G_R
-
-# Do a cut to get rid of giants
-mask = (G_B > 4) & (GminR > 0) & (GminR < 4.5)
+G_mag = iso['Gmag'].values[::-1]
+color = G_mag - G_R
 
 # Interpolate gaia color onto isochrone to get TESS mag
-Gbp_min_Grp = tess_gaia['phot_g_mean_mag'] - tess_gaia['phot_rp_mean_mag']
-TESS_mag_int = np.interp(Gbp_min_Grp, G_B[mask] - G_R[mask], T[mask])
+G_min_Grp = tess_gaia['phot_g_mean_mag'] - tess_gaia['phot_rp_mean_mag']
+
+# The cutting gets a little funky here. Need to cleanly cut out the main sequence part of the isochrone, otherwise
+# the interpolation doesn't work correctly. Also need to grab the next point past the end of the color cut, otherwise
+# the interpolated model ends up with a flat spot in the CMD on the right side of the main sequence
+color_min, color_max = 0.2, 1.4
+iso_width = 0.4 # How far above and below the isochrone to go to select MS stars
+mask_iso = (G_mag > 2) & (G_mag - G_R > color_min) & (G_mag - G_R < 1.45) # Go a little past the color limit to get the next point in the isochrone model
+mask_real = (G_min_Grp > color_min) & (G_min_Grp < color_max)
+TESS_mag_int = np.interp(G_min_Grp, G_mag[mask_iso] - G_R[mask_iso], T[mask_iso])
+G_abs_mag_int = np.interp(G_min_Grp[mask_real], G_mag[mask_iso] - G_R[mask_iso], G_mag[mask_iso])
 
 # Convert TESS mag to luminosity
 Tf0 = 4.03e-6*u.erg/u.s/u.cm**2 # Zero point TESS flux (from Sullivan 2017)
@@ -93,9 +100,11 @@ m_t = TESS_mag_int + 5*np.log10(dist.value) - 5
 f = 10**(-m_t/2.5)*Tf0
 L = 4*np.pi*(dist.to(u.cm))**2*f
 
+ms_mask = (G_min_Grp[mask_real] > color_min) & (G_min_Grp[mask_real] < color_max) & (G_abs_mag[mask_real] < G_abs_mag_int + iso_width) & (G_abs_mag[mask_real] > G_abs_mag_int - iso_width)
+
 tess_gaia_table = pd.DataFrame({'TIC':tics, 'r_est':dist, 'lum':L, 'G_BPbrmag':tess_gaia['phot_bp_mean_mag'], \
                       'G_RPmag':tess_gaia['phot_rp_mean_mag'], 'G_mag':tess_gaia['phot_g_mean_mag'], 'G_abs_mag':G_abs_mag,\
-                       'source_id':tess_gaia['source_id'], 'ra':tess_gaia['ra'], 'dec':tess_gaia['dec']})
+                       'source_id':tess_gaia['source_id'], 'ra':tess_gaia['ra'], 'dec':tess_gaia['dec'], 'is_ms': np.isin(tics, tics[mask_real][ms_mask])})
 
 print(str(len(tess_gaia_table)) + ' targets found in TESS-Gaia crossmatch')
 
@@ -110,10 +119,13 @@ df_param_g = pd.merge(tess_gaia_table, df_param, on='TIC', how='inner')
 
 # Throw out bad luminosity measurements
 df_flare_g = df_flare_g.dropna(subset=['lum'])
-df_flare_g = df_flare_g.dropna(subset=['lum'])
 ed = (df_flare_g['ed'].values*u.day).to(u.s)
 energy = (ed*df_flare_g['lum']*u.erg/u.s).value
 df_flare_g['energy'] = energy
+
+ed_err = (df_flare_g['ed_err'].values*u.day).to(u.s)
+energy_err = (ed_err*df_flare_g['lum']*u.erg/u.s).value
+df_flare_g['energy_err'] = energy_err
 
 # TODO: Filter out MS stars by color and mag
 
@@ -130,7 +142,7 @@ mask = (df['skew'] > 0.5) & \
        (df['f_fwhm_win'] < 0.1) &(df['g_fwhm_win'] < 0.1) & \
        (df['ed'] > 0) & (df['tpeak'] > df['t0']) & (df['tpeak'] < df['t1']) & \
        (df['cover'] > 0.9) & \
-       (df['f_chisq'] < df['g_chisq'])
+       (df['f_chisq'] < 0.7*df['g_chisq'])
 
 df = df[mask]
 print(str(len(df)) + ' flare events from ' + str(len(np.unique(df['TIC']))) + ' targets in ' + str(len(np.unique(df['file']))) + ' lightcurves')
